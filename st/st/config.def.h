3f1a9a5fea4f9c4f72ddc476ba68dc1a8baefd2c
rlkandela <rlkandela@gmail.com>
2019-11-20 13:47:11 +0100

    added all st files after alpha + font patches

--- st/st/config.def.h
+++ st/st/config.def.h
@@ -5,14 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true", 
-	"Hack Nerd Font Mono:pixelsize=15:antialias=true:autohint=true", 
-	"Font Awesome 5 Brands:pixelsize=15",
-	"Font Awesome 5 Free Solid:pixelsize=15",
-	"Font Awesome 5 Free:pixelsize=15"
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 
 static int borderpx = 2;
@@ -91,7 +88,7 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 /* bg opacity */
-float alpha = 0.5;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
